6ab0e8135b7f53f8e72b389ed051669b68687a42
Delfina Moncayo Account <delfina.moncayo@gmail.com>
2020-04-06 14:08:44 -0400

    fix validator

--- js/scripts.js
+++ js/scripts.js
@@ -35,12 +35,12 @@ fetch(requestURL)
   })
   .then( function (jsonObject) {
    
-    const prophets = jsonObject['prophets'];
+    const guides = jsonObject['guides'];
     
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < guides.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');      
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname; 
+        h2.textContent = guides[i].name + ' ' + prophets[i].lastname; 
         card.appendChild(h2); 
         let p = document.createElement('p');      
         p.innerHTML = 'Date of Birth: ' + prophets[i].birthdate + '<br/>Place of Birth: ' + prophets[i].birthplace;
@@ -50,7 +50,8 @@ fetch(requestURL)
         image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order );  
         card.appendChild(image);
         document.querySelector('div.cards').appendChild(card);
-        
+      
      
     }
   })
+}
