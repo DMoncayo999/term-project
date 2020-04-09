4781f0154c1f3fe165809158b29e6599c9c99ea2
Delfina Moncayo Account <delfina.moncayo@gmail.com>
2020-04-08 22:25:15 -0400

    update weather api

--- js/weatherapi.js
+++ js/weatherapi.js
@@ -1,18 +1,24 @@
 // store the resource URL of the JSON
 
-let populateWeather = () => {
+const populateWeather = () => {
 
    // const weatherapiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=e133193565792698f423478349cebc85&units=imperial";
 
     //Go fetch it and then wait for a response.
-    fetch(weatherapiURL)
-    .then((response) => response.json())
-    .then((weatherInfo) => {
-        console.log (weatherInfo)
-        document.getElementById('name').innerHTML = weatherInfo.weather.name;
-        document.getElementById('description').innerHTML = weatherInfo.weather[0].description;
-        document.getElementById('icon').innerHTML = (weatherInfo.weather[0].icon);
-        document.getElementById('temperature').innerHTML = (weatherInfo.main.temp).toFixed(0);
+    fetch( weatherapiURL )
+    .then( (response) => response.json() )
+    .then( (weatherInfo) => {
+        console.log(weatherInfo)
+        document.getElementById('weatherCity').innerHTML = weatherInfo.name;
+        document.getElementById('weatherLike').innerHTML = weatherInfo.weather[0].description;
+        // document.getElementById('weatherIcon').innerHTML = weatherInfo.weather[0].icon;
+        let iconcode = weatherInfo.weather[0].icon;
+        let iconPath = "https://openweathermap.org/img/w/" + iconcode + ".png";
+        let theicon = document.createElement ('img');
+        theicon.src = iconPath;
+        theicon.alt = iconcode;
+        document.getElementById("weatherIcon").appendChild(theicon);
+        document.getElementById('weatherTemperature').innerHTML = (weatherInfo.main.temp).toFixed(0) + '&deg;F';
                 
     }); 
 }
